3639614c9c52f137689afe18c7b1cf98a2df005f
kidus-m <kidus0237@gmail.com>
2025-12-19 19:35:46 +0300

    seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 
 import "@/styles/globals.css";
-
+import Head from "next/head";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+  return <>
+    <Head>
+      <meta name="google-site-verification" content="LNwSQuzOjNMQo2bGOXwZXmYFtDpCtH6K29K9QzXlQ9k" />
+    </Head>
+    <Component {...pageProps} /></>;
 }
